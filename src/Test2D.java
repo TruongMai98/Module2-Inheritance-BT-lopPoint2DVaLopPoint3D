4af539b91516f909170e27e8b0cf63583386e1b3
TruongMai <112840965+TruongMai98@users.noreply.github.com>
2022-10-13 16:04:41 +0700

    lop point 2d va 3d

---
+++ src/Test2D.java
@@ -0,0 +1,12 @@
+public class Test2D {
+    public static void main(String[] args) {
+        Point2D point1 = new Point2D();
+
+        point1.getXY(2.0f,3.4f);
+        System.out.print("\n");
+        point1.setX(1.0f);
+        point1.setY(1.34f);
+
+        System.out.println(point1);
+    }
+}
